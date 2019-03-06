90f0eca9c47748f41dc491c80872f0b88371c42a
stevsal <stevsal@users.noreply.github.com>
2019-03-06 15:19:32 +0200

    Lisatud lahendused

--- Main.java
+++ Main.java
@@ -14,11 +14,8 @@ public class Main {
     arvud[3] = 9;
     arvud[4] = 1;
     arvud[5] = 2;
-    //arvud[suurus+1] = 0;
-    //arvud[suurus+2]= 0;
     //int v2him = arvud[0];
     arvud = j2rjestaja(arvud,suurus);
-
 
     System.out.println("Masiivi järjestatult on ");
     for (int i=0; i< suurus;i++){
@@ -34,12 +31,16 @@ public class Main {
 
   public static int[] j2rjestaja(int[] arvud, int suurus){
 
+
     int v2ike = arvud[0];
     int suur = 0;
     int vahetusi = 1;
     while(vahetusi ==1){
       vahetusi = 0;
       for(int i=0; i < suurus-1;i++){
+        if(suur<arvud[i]){
+          suur=arvud[i];
+        }
         if(arvud[i]>arvud[i+1]){
           int abi = arvud[i];
           arvud[i] = arvud[i+1];
@@ -47,18 +48,20 @@ public class Main {
           vahetusi=1;
         }
       if(v2ike>arvud[i]){v2ike=arvud[i];}
-      if(suur<arvud[i]){suur=arvud[i];/*System.out.println("Suur arv on "+arvud[i]);*/}
-
     }
   }
-  int veerand = (int)Math.ceil((suurus/100.00)*25);
-  int pool = (int)Math.ceil((suurus/100.00)*50);
-  int kolmveera = (int)Math.ceil((suurus/100.00)*75);
-  System.out.println("v"+veerand+" p"+pool+" k"+kolmveera);
-  arvud[suurus+2] = arvud[veerand-1];
-  arvud[suurus+3] = arvud[pool-1];
-  arvud[suurus+4] = arvud[kolmveera-1];
-
+    /*for(int j=0;j<suurus;j++){
+      if(suur<arvud[j]){
+        suur=arvud[j];
+      }
+    }*/
+    int veerand = (int)Math.ceil((suurus/100.00)*25);
+    int pool = (int)Math.ceil((suurus/100.00)*50);
+    int kolmveera = (int)Math.ceil((suurus/100.00)*75);
+    System.out.println("v"+veerand+" p"+pool+" k"+kolmveera);
+    arvud[suurus+2] = arvud[veerand-1];
+    arvud[suurus+3] = arvud[pool-1];
+    arvud[suurus+4] = arvud[kolmveera-1];
     arvud[suurus] = v2ike;
     arvud[suurus+1] = suur;
     //System.out.println("Suurim arv on "+arvud[suurus-1]);
