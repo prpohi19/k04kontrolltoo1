fd027298f5389b2935947cc59e2b824377555ee6
Urmot Rosenberg <43164656+urmotr@users.noreply.github.com>
2019-03-06 13:05:29 +0200

    Harmooniline keskmine

---
+++ teine.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class teine {
+	
+	public static double harmooniline_keskmine(ArrayList<Integer> kiirused) {
+		double keskmine = 0; 
+		double harmooniline_keskmine = 0;
+		for (int k : kiirused) {
+			keskmine += (1.0/k);
+		}
+		harmooniline_keskmine = kiirused.size()/ keskmine;
+		return harmooniline_keskmine;
+	}
+	
+	public static void main(String[] arg) throws Exception{
+		double harmooniline_keskmine = 0;
+		ArrayList<Integer> kiirused = new ArrayList<Integer>();
+		BufferedReader lugeja=new BufferedReader(new FileReader("C:\\Users\\Student Admin\\eclipse-workspace\\Harmooniline keskmine\\src\\kiirused2.sis"));
+		String rida=lugeja.readLine();
+		while(rida!=null) {
+			kiirused.add(Integer.parseInt(rida));
+			rida=lugeja.readLine();
+		}
+		lugeja.close();
+		harmooniline_keskmine = harmooniline_keskmine(kiirused);
+		System.out.println(harmooniline_keskmine);
+	}
+}
