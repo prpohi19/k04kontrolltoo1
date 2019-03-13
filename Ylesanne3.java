4cf02f193d55d1ecfc6436163e98f80eebe28541
Priit Laupa <priilau@users.noreply.github.com>
2019-03-13 12:25:48 +0200

    kolmas tehtud

--- Ylesanne3.java
+++ Ylesanne3.java
@@ -2,50 +2,50 @@ import java.util.*;
 import java.io.*;
 
 public class Ylesanne3{
-  public static double calcGeo(double[] geo){
+  public static double calcGeo(double[] salaryCoef){
     double product = 0;
-    double answer = 0;
-    double amount = geo.length;
+    double amount = salaryCoef.length;
     double power = 1 / amount;
     for (int i = 0; i < amount; i++){
       if (i == 0){
-        product = geo[i];
+        product = salaryCoef[i];
       } else {
-          product = product * geo[i];
+          product = product * salaryCoef[i];
       }
     }
-    answer = Math.pow(product, power);
-    return answer;
+    return Math.pow(product, power);
   }
 
   public static void main(String[] args) throws Exception{
-    BufferedReader reader = new BufferedReader(new FileReader("palgadsis2.txt"));
+    BufferedReader reader = new BufferedReader(new FileReader("palgadsis.txt"));
     String info = reader.readLine();
     String[] infoSplit = info.split(" ");
     double baseSalary = Double.parseDouble(infoSplit[0]);
     int amount = Integer.parseInt(infoSplit[1]);
-    String[] salaryCoef = new String[amount];
-    double[] geo = new double[amount];
+    double[] salaryCoef = new double[amount];
     List<String> calculatedSalaries = new ArrayList<String>();
 
     for(int i = 0; i < amount; i++){
-      salaryCoef[i] = reader.readLine();
+      salaryCoef[i] = Double.parseDouble(reader.readLine());
     }
+
+    double geo = calcGeo(salaryCoef);
+    double geoSalary = baseSalary;
+
     for(int i = 0; i <= amount; i++){
       if(i != 0){
-        double coef = Double.parseDouble(salaryCoef[i-1]);
-        double check = Math.round(baseSalary * coef);
-        geo[i] = baseSalary * check;
-        calcGeo(geo[i], i);
-        baseSalary = check;
-        calculatedSalaries.add(String.valueOf(check) + " | " + String.valueOf(geoCheck));
+        double coef = salaryCoef[i-1];
+        double salary = Math.round(baseSalary * coef);
+        geoSalary = Math.round(geoSalary * geo);
+        baseSalary = salary;
+        calculatedSalaries.add(String.valueOf(salary) + " | " + String.valueOf(geoSalary));
       } else {
-        calculatedSalaries.add("Baas palk: " + String.valueOf(baseSalary));
-        geo[i] = baseSalary;
+        calculatedSalaries.add("Baas palk: " + baseSalary);
+        calculatedSalaries.add("Koef kesk | geom kesk(" + geo +")");
       }
     }
-    PrintWriter writer = new PrintWriter(new FileWriter("palgadval2.txt"));
-    writer.println("Koef kesk | geom kesk");
+
+    PrintWriter writer = new PrintWriter(new FileWriter("palgadval.txt"));
     for (int i = 0; i < calculatedSalaries.size(); i++){
       writer.println(calculatedSalaries.get(i));
     }
